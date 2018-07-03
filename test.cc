ffded7c7d8cbd14c3c5e80a8a8f91b7c8ead9900
wenruo95 <1358124684@qq.com>
2018-07-03 22:54:38 +0800

    fix

--- test.cc
+++ test.cc
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <map>
  
+using std::map;
+
 int main()
 {  
 	std::cout << __cplusplus << std::endl;
-	/*
-	std::map<int,char> example = {{1,'a'},{2,'b'}};
-	if(example.contains(2)) {
+
+	std::map<int,char> m = {{1,'a'},{2,'b'}};
+	// m.contains c++20
+	if(m.find(2) != m.end()) {
 		std::cout << "Found\n";
 	} else {
 		std::cout << "Not found\n";
 	}
-	*/
 	return 0;
 }
