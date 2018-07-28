fb149ca78a9097d116fb2e5803e4e26f3fc143bb
wenruo95 <1358124684@qq.com>
2018-07-28 15:51:28 +0800

    新增stl测试用例

--- stl/test-stl.cc
+++ stl/test-stl.cc
@@ -19,6 +19,14 @@ int main() {
 	m[9] = set2;
 	m[8] = set2;
 
+	std::set<int> set3 = m[9];
+	set3.insert(11);
+	m[10] = set3;
+
+	std::set<int> set4 = m[7];
+	set4.insert(12);
+	m[7] = set4;
+
 
 	for (std::map<int,std::set<int>>::iterator it = m.begin(); it != m.end(); it++) {
 		std::cout << "int:\t" << it->first << std::endl;
@@ -26,5 +34,11 @@ int main() {
 		for (std::set<int>::iterator it2 = mset.begin(); it2 != mset.end(); it2++) {
 			std::cout << "set_int:\t" << *it2 << std::endl;
 		}
+	}
+
+	std::map<int,std::set<int>>::iterator first = m.begin();
+	std::cout << first->first << std::endl;
+	for (std::set<int>::iterator it = (first->second).begin(); it != (first->second).end(); it++) {
+		std::cout << "\t" << *it << std::endl;
 	}
 }
