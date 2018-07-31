1a18975a30f7fade9d6b649ea6c807d93a0d8312
wenruo95 <1358124684@qq.com>
2018-07-31 21:32:50 +0800

    add link

--- base64/base64.cc
+++ base64/base64.cc
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 
+// 参考链接1:https://zh.wikipedia.org/wiki/Base64
+// 链接2:http://tool.oschina.net/encrypt?type=3
 // 3个字节编译成4个字符 [0,25](A-Z) [26,51](a-z) [52,61](0,9) [62,63](+,/)
 // 加密
 std::string encrypt_base64(const std::string str);
