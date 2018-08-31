31f47f6d36ae460d95c41d09f40f06bc516f2cc2
wenruo95 <1358124684@qq.com>
2018-08-31 17:59:46 +0800

    atoi

--- leetcode/atoi.cc
+++ leetcode/atoi.cc
@@ -6,35 +6,76 @@ using std::string;
 
 class Solution {
 public:
-	int reverse(string str) {
-		if (x == 0) {
-			return x;
+	int myAtoi(string str) {
+		if (str.size() == 0) {
+			return 0;
 		}
-		unsigned int real_num = (x > 0) ? x : -x;
+		int left = -1, right = 0;
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == '-' || str[i] == '+') {
+				if (left == -1) {
+					left = i;
+					continue;
+				} else {
+					break;
+				}
+			}
+			if (str[i] == ' ') {
+				if (left == -1) {
+					continue;
+				} else {
+					break;
+				}
+			}
+			if (str[i] > '9' || str[i] < '0') {
+				break;
+			}
+			if (left == -1) {
+				left = i;
+			}
+			right = i;
+		}
+
+		// 首个为字母
+		if (left == -1 || right < left) {
+			return 0;
+		}
+		int x = (str[left] == '-') ? -1 : 1;
+		if (str[left] == '-' || str[left] == '+') {
+			left++;
+		}
+		//
+		//std::cout << str << "\tleft:\t" << left << "\trigth:\t" << right << std::endl;
+		std::string chs(&str[left], &str[right + 1]);
 		unsigned int head = (x > 0) ? (INT_MAX / 10) : -(INT_MIN / 10);
 		unsigned int tail = (x > 0) ? (INT_MAX % 10) : -(INT_MIN % 10);
 		int result = 0;
-		while (real_num > 0) {
-			unsigned int last_num = real_num % 10;
+		for (int i = 0; i < chs.size(); i++) {
+			unsigned int last_num = chs[i] - '0';
 			if (result > head || (result == head && last_num > tail)) {
-				return 0;
+				return x > 0 ? INT_MAX : INT_MIN;
 			}
 			result = result * 10 + last_num;
-			real_num = real_num / 10;
 		}
-		return (x > 0) ? result : -result;
+		return x * result;
 	}
 };
 
 int main(void) {
-	std::cout << INT_MIN << "\t" << INT_MAX << std::endl;
 	Solution solution;
-	int a = 123;	// 321
-	int b = -123;	// -321
-	int c = 120;	// 21
-	int d = 1534236469;
-	std::cout << a << "\t" << solution.reverse(a) << std::endl;
-	std::cout << b << "\t" << solution.reverse(b) << std::endl;
-	std::cout << c << "\t" << solution.reverse(c) << std::endl;
-	std::cout << d << "\t" << solution.reverse(d) << std::endl;
+	std::string str1 = "42"; // 42
+	std::string str2 = "    -42"; // -42
+	std::string str3 = "4193 with words"; // 4193
+	std::string str4 = "words and 987"; // 0
+	std::string str5 = "-91283472332"; // -2147483648
+	std::string str6 = "   - 321"; // -2147483648
+	std::string str7 = "+1"; // -2147483648
+	std::cout << str1 << "\t" << solution.myAtoi(str1) << std::endl;
+	std::cout << str2 << "\t" << solution.myAtoi(str2) << std::endl;
+	std::cout << str3 << "\t" << solution.myAtoi(str3) << std::endl;
+	std::cout << str4 << "\t" << solution.myAtoi(str4) << std::endl;
+	std::cout << str5 << "\t" << solution.myAtoi(str5) << std::endl;
+	std::cout << str6 << "\t" << solution.myAtoi(str6) << std::endl;
+	std::cout << str7 << "\t" << solution.myAtoi(str7) << std::endl;
 }
+
