d6bb98a2ee8d4832d9ef1d021ffc71bcfd21efa9
wenruo95 <1358124684@qq.com>
2018-10-11 22:30:08 +0800

    exception

--- abc/exception.cc
+++ abc/exception.cc
@@ -1,6 +1,28 @@
 #include<iostream>
 
 //http://www.runoob.com/cplusplus/cpp-exceptions-handling.html
+/*
+ * std::exception
+ * std::bad_alloc
+ * std::bad_cast
+ * std::bad_exception
+ * std::bad_typeid
+ * std::logic_error
+ * std::domain_error
+ * std::invalid_argument
+ * std::length_error
+ * std::out_of_range
+ * std::runtime_error
+ * std::overflow_error
+ * std::range_error
+ * std::underflow_error
+ */
+
+struct MyException: public std::exception {
+	const char* what() const throw() {
+		return "C++ Exception";
+	}
+};
 
 double division(int a, int b) {
 	if (b == 0) {
@@ -18,5 +40,13 @@ int main(void) {
 	} catch( const char* msg) {
 		std::cout << "test catch" << std::endl;
 		std::cerr << msg << std::endl;
+	}
+
+	try {
+		throw MyException();
+	} catch (MyException e) {
+		std::cout << e.what() << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "exception" << std::endl;
 	}
 }
